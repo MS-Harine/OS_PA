8e0ac2b23b2beadc41b4ea3f2fe703264ffd050b
MS-Harine <21700371@handong.edu>
2019-03-27 03:22:22 +0900

    Change p3

--- p3/hideme.c
+++ p3/hideme.c
@@ -1,27 +1,91 @@
+#include <linux/syscalls.h>
 #include <linux/module.h>
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
 #include <linux/uaccess.h>
+#include <linux/kallsyms.h>
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <asm/unistd.h>
+
+#include <linux/string.h>
 
 MODULE_LICENSE("GPL");
 
-static 
-int __init baremin_init(void) {
-	struct module *target_module;
-	printk("Hideme : Hi.\n") ;
-	
-	list_for_each_entry(target_module, &THIS_MODULE->list, list) {
-		if (target_module->init == baremin_init)
-			printk("Module list: %s\n", target_module->name);
-	}
+#define	TURE	1
+#define FALSE	0
 
+int is_hide = FALSE;
+struct list_head *this = NULL;
+
+static int hideme_proc_open(struct inode *inode, struct file *file) {
 	return 0;
 }
 
-static 
-void __exit baremin_exit(void) {
-	printk("Hideme : Bye.\n") ;
+static int hideme_proc_release(struct inode *inode, struct file *file) {
+	return 0;
 }
 
-module_init(baremin_init);
-module_exit(baremin_exit);
+static ssize_t hideme_proc_read(struct file *file, char __user *ubuf, size_t size, loff_t *offset) {
+	char buf[256];
+	ssize_t toread;
+
+	sprintf(buf, "Hide status: %d\n", is_hide);
+	toread = strlen(buf) >= *offset + size ? size : strlen(buf) - *offset;
+
+	if (copy_to_user(ubuf, buf + *offset, toread))
+		return -EFAULT;
+
+	*offset = *offset + toread;
+	return toread;
+}
+
+static ssize_t hideme_proc_write(struct file *file, const char __user *ubuf, size_t size, loff_t *offset) {
+	char buf[128];
+	struct list_head module_list;
+
+	if (*offset != 0 || size > 128)
+		return -EFAULT;
+
+	if (copy_from_user(buf, ubuf, size))
+		return -EFAULT;
+
+	if (is_hide) {
+		THIS_MODULE->list->prev->next = THIS_MODULE->list;
+		THIS_MODULE->list->next->prev = THIS_MODULE->list;
+	}
+	else {
+		THIS_MODULE->list->prev->next = THIS_MODULE->list->next;
+		THIS_MODULE->list->next->prev = THIS_MODULE->list->prev;
+	}
+
+	is_hide = !is_hide;
+	*offset = strlen(buf);
+	return *offset;
+}
+
+static const struct file_operations hideme_fops = {
+	.owner =	THIS_MODULE,
+	.open =		hideme_proc_open,
+	.read =		hideme_proc_read,
+	.write =	hideme_proc_write,
+	.llseek =	seq_lseek,
+	.release =	hideme_proc_release,
+};
+
+static int __init hideme_init(void) {
+	proc_create("hideme", S_IRUGO | S_IWUGO, NULL, &hideme_fops);
+	return 0;
+}
+
+static void __exit hideme_exit(void) {
+	if (is_hide) {
+		THIS_MODULE->list->prev->next = THIS_MODULE->list;
+		THIS_MODULE->list->next->prev = THIS_MODULE->list;
+	}
+
+	remove_proc_entry("hideme", NULL);
+}
+
+module_init(hideme_init);
+module_exit(hideme_exit);
